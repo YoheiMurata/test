0077f0ec00b32f2efd7f0e46273c03ecd0ad603d
Yohei <fullmetaljacket893@gmail.com>
2013-05-01 21:24:16 +0900

    first commit

---
+++ test.c
@@ -0,0 +1,12 @@
+#################################
+# test.c			#
+# テスト。特に意味はない	#
+#################################
+
+#include <stdio.h>
+
+int main( int args, char *argv[] )
+{
+	printf( "hello, world\n" );
+	return 0;
+}
